4f9cac4d785d407bd8649456567e86a9ffe6697b
Fiorela <fchavarria@estudiantec.cr>
2024-05-04 13:46:57 -0600

    Actualizando

--- UART/ControlReg.sv
+++ UART/ControlReg.sv
@@ -6,8 +6,12 @@ module ControlReg (
     input logic [31:0] IN2,
     input logic WR1,
     input logic WR2,
-    output logic out
+    output logic [31:0] out
 );
 
-
+always_ff @(posedge clk)begin 
+    if (rst) out <= 'b0 ;
+    else if (WR1) out <= IN1;
+    else if (WR2) out <= IN2;
+end
 endmodule
