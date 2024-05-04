3499a9ac3c6a9bded3fb4c14d0ca966243d20308
Fiorela <fchavarria@estudiantec.cr>
2024-05-04 13:33:43 -0600

    Actualizando

--- UART/ControlReg.sv
+++ UART/ControlReg.sv
@@ -1,7 +1,13 @@
 `timescale 1ns / 1ps
 
 module ControlReg (
-
-
+    input logic clk, rst,
+    input logic [31:0] IN1,
+    input logic [31:0] IN2,
+    input logic WR1,
+    input logic WR2,
+    output logic out
 );
+
+
 endmodule
