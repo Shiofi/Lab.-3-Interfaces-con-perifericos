9c8af0682a2f6d6df684f2695217affdb19a4ed4
Fiorela <fchavarria@estudiantec.cr>
2024-05-06 14:39:01 -0600

    Actualizando

---
+++ UART/TB/tb_ControlFSM_UART.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/06/2024 01:57:10 PM
+// Design Name: 
+// Module Name: tb_ControlFSM_UART
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_ControlFSM_UART(
+
+    );
+endmodule
