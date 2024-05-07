5656fcf7761ef673b503bf0221c1b1dd52314c20
RoloVega <100702229+RoloVega@users.noreply.github.com>
2024-05-07 14:41:23 -0600

    Update mux.sv

--- SPI/sources/mux.sv
+++ SPI/sources/mux.sv
@@ -1,10 +1,11 @@
-module mux_2_to_1 (
-    input wire clk,
-    input wire reset,
-    input wire sel,
-    input wire [31:0] in1,
-    input wire [31:0] in2,
-output reg [31:0] out
+`timescale 1ns / 1ps
+module mux_2_to_1#(parameter N=32) (
+    input logic clk,
+    input logic reset,
+    input logic sel,
+    input logic [N-1:0] in1,
+    input logic [N-1:0] in2,
+output logic [N-1:0] out
 );
 
 always @(posedge clk or posedge reset) begin
